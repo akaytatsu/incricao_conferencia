d352bf537ced27ea49079ec5b998f696ab014dc0
Thiago Freitas <thiagosistemas3@gmail.com>
2019-12-02 22:23:56 -0300

    Models

--- apps/data/admin.py
+++ apps/data/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Conferencia, Valores, Hospedagem, Inscricao, Dependente
 
-# Register your models here.
+@admin.register(Conferencia)
+class ConferenciaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'max_inscr', 'data_abertura', 'data_encerramento', )
+    search_fields = ('titulo', )
+
+@admin.register(Valores)
+class ValoresAdmin(admin.ModelAdmin):
+    list_display = ('conferencia', 'idade_inicial', 'idade_final', 'valor', )
+    search_fields = ('conferencia', 'idade_inicial', )
+    list_filter = ('conferencia', 'valor', )
+
+@admin.register(Hospedagem)
+class HospedagemAdmin(admin.ModelAdmin):
+    list_display = ('conferencia', 'nome', 'limite', 'ativo', )
+    search_fields = ('conferencia', 'nome', )
+    list_filter = ('conferencia', 'limite', )
+
+@admin.register(Inscricao)
+class InscricaoAdmin(admin.ModelAdmin):
+    list_display = ('conferencia', 'cpf', 'nome', 'nome_cracha', 'data_nascimento', 'email', 'idade', 'hospedagem', 'valor', 'valor_total', )
+    search_fields = ('conferencia', 'cpf', 'nome', 'email')
+    list_filter = ('conferencia', 'hospedagem', 'idade', )
+
+@admin.register(Dependente)
+class DependenteAdmin(admin.ModelAdmin):
+    list_display = ('inscricao', 'nome', 'nome_cracha', 'data_nascimento', 'grau', 'valor', )
+    search_fields = ('inscricao', 'nome', )
+    list_filter = ('inscricao', 'grau', )
--- apps/data/models.py
+++ apps/data/models.py
@@ -14,6 +14,9 @@ class Conferencia(models.Model):
         verbose_name = 'Conferencia'
         verbose_name_plural = 'Conferencias'
 
+    def __str__(self):
+        return self.titulo
+
 class Valores(models.Model):
 
     conferencia = models.ForeignKey(Conferencia, on_delete=models.CASCADE, verbose_name="Conferência")
@@ -26,7 +29,25 @@ class Valores(models.Model):
         verbose_name = 'Valor Inscrição Conferencia'
         verbose_name_plural = 'Valores Inscrição Conferencia'
 
-class inscricao(models.Model):
+    def __str__(self):
+        return "{} - R$ {} de {} a {}".format(self.conferencia, self.valor, self.idade_inicial, self.idade_final)
+
+class Hospedagem(models.Model):
+
+    conferencia = models.ForeignKey(Conferencia, on_delete=models.CASCADE, verbose_name="Conferência")
+    nome = models.CharField(max_length=9, verbose_name="Nome Hospedagem")
+    limite = models.IntegerField(default=0, verbose_name="Limite de Hospedes")
+    ativo = models.BooleanField(default=True, verbose_name="Ativo?")
+
+    class Meta:
+        db_table = "hospedagem_conferencia"
+        verbose_name = 'Hospedagem Conferência'
+        verbose_name_plural = 'Hospedagem Conferência'
+
+    def __str__(self):
+        return "{} - {}".format(self.conferencia, self.nome)
+
+class Inscricao(models.Model):
 
     conferencia = models.ForeignKey(Conferencia, on_delete=models.CASCADE, verbose_name="Conferência")
     cpf = models.CharField(max_length=11, verbose_name="CPF")
@@ -34,12 +55,51 @@ class inscricao(models.Model):
     nome_cracha = models.CharField(max_length=100, verbose_name="Nome Crachá")
     data_nascimento = models.DateField(verbose_name="Data Nascimento")
     email = models.CharField(max_length=120, verbose_name="E-mail")
-    # aaaaaaaa = models.CharField(max_length=120, verbose_name="aaaaaaaaaaaaaaaaaa")
-    # aaaaaaaa = models.CharField(max_length=100, verbose_name="aaaaaaaaaaaaaaaaaa")
-    # aaaaaaaa = models.CharField(max_length=100, verbose_name="aaaaaaaaaaaaaaaaaa")
+    cep = models.CharField(max_length=9, verbose_name="CEP")
+    endereco = models.CharField(max_length=120, verbose_name="Endereço")
+    numero = models.CharField(max_length=60, verbose_name="Numero")
+    complemento = models.CharField(max_length=60, verbose_name="Complemento")
+    bairro = models.CharField(max_length=80, verbose_name="Bairro")
+    cidade = models.CharField(max_length=80, verbose_name="Cidade")
+    uf = models.CharField(max_length=2, verbose_name="UF")
+    ddd = models.CharField(max_length=3, verbose_name="DDD")
+    telefone = models.CharField(max_length=30, verbose_name="Telefone")
+    observacoes = models.CharField(max_length=400, verbose_name="Observações")
+    hospedagem = models.ForeignKey(Hospedagem, on_delete=models.CASCADE, verbose_name="Hospedagem")
+    valor = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor Inscrição")
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor Total Inscrição")
+    idade = models.IntegerField(default=0, verbose_name="Idade")
     
-
     class Meta:
+        unique_together = [['conferencia', 'cpf']]
         db_table = "inscricao"
         verbose_name = 'Inscrição'
         verbose_name_plural = 'Inscrições'
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.conferencia, self.cpf, self.nome)
+
+class Dependente(models.Model):
+
+    _GRAU = (
+        (1, 'Conjugue'),
+        (2, 'Filho(a)'),
+        (3, 'Pai/Mãe'),
+        (4, 'Outro'),
+    )
+
+    inscricao = models.ForeignKey(Inscricao, on_delete=models.CASCADE, verbose_name="Inscrição")
+    nome = models.CharField(max_length=100, verbose_name="Nome Completo")
+    grau = models.CharField(choices=_GRAU, max_length=30, verbose_name="Grau Parentesco")
+    nome_cracha = models.CharField(max_length=100, verbose_name="Nome Crachá")
+    data_nascimento = models.DateField(verbose_name="Data Nascimento")
+    idade = models.IntegerField(default=0, verbose_name="Idade")
+    valor = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor Inscrição")
+
+    class Meta:
+        db_table = "dependente"
+        verbose_name = 'Dependente'
+        verbose_name_plural = 'Dependentes'
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.inscricao, self.grau, self.nome)
