3f4c3b97f8fb5071fb19c4342e84205eb5b1dc06
Thiago Freitas <thiagosistemas3@gmail.com>
2019-12-21 11:12:25 -0300

    corre~]ao

--- conf/settings.py
+++ conf/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config
-import pymysql
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_mysql',
     'rest_framework',
     'widget_tweaks',
     'localflavor',
